09dd980b81f528b011c0f84358a4899d0e138285
Richter Eisen <fish@DESKTOP-HE8C5RN>
2025-11-23 20:51:00 -0600

    add in execution logic

--- miniShell.c
+++ miniShell.c
@@ -7,26 +7,33 @@ void printPrompt()
 
 void readCommand(char *commandLine)
 {
+    // Print prompt string to the stdout
     printPrompt();
+    // Get user input
     fgets(commandLine, MAX_LINE_LEN, stdin);
+    // Get the length of the input string
     size_t len = strlen(commandLine);
+    // Remove trailing newline character
+    // and put in a null character as the last character
     if (len > 0 && commandLine[len - 1] == '\n')
     {
         commandLine[len - 1] = '\0';
     }
-
-    // TODO: REMOVE PRINT STATEMENT
-    printf("Command Entered: %s\n", commandLine);
 }
 
 int parseCommand(char *cLine, Command *cmd)
 {
+    /*
+     * This function parses the command line string, cLine,
+     * into its component parts: command name and arguments.
+     * It fills in the Command structure pointed to by cmd.
+     */
     int argc;
     char **clPtr;
-
-    // Initialization
     clPtr = &cLine;
     argc = 0;
+
+    // Allocate space for argv[]
     cmd->argv[argc] = (char *)malloc(MAX_ARG_LEN);
 
     // Fill argv[]
@@ -86,11 +93,52 @@ int parsePath(char *dirs[])
 
 char *lookupPath(char **argv, char **dir)
 {
-    // TODO: Implement lookupPath function
+    /*
+    This function searches the directories identified by the dir
+    argument to if argv[0] (the file name) appears there.
+    Allocates a new string, place the fill path name in it, then
+    return the string.
+    */
+
+    char *result;
+    char pName[MAX_PATH_LEN];
+    int i;
+
+    // Check to see if file name is already an absolute path
+    if (argv[0][0] == '/')
+    {
+        // It's an absolute path, check if it exists
+        if (access(argv[0], F_OK) == 0)
+        {
+            return strdup(argv[0]);
+        }
+        else // File doesn't exist
+        {
+            return NULL;
+        }
+    }
+
+    // search through the PATH directories
+    for (i = 0; dir[i] != NULL; i++)
+    {
+        // Build the full path name
+        snprintf(pName, sizeof(pName), "%s/%s", dir[i], argv[0]);
+
+        // Check if the file exists at this path
+        if (access(pName, F_OK) == 0)
+        {
+            // File found, allocate memory for result and return it
+            result = (char *)malloc(strlen(pName) + 1);
+            strcpy(result, pName);
+            return result;
+        }
+    }
 }
 
 int main()
 {
+
+    // Declare variables
     int pid;
     char commandLine[MAX_LINE_LEN];
     char *pathv[MAX_PATH_LEN];
@@ -102,8 +150,19 @@ int main()
     // Execution loop
     while (1)
     {
-        // Read command line
+        // Read command line input
         readCommand(commandLine);
+
+        // If command is "exit", then quit
+        if (strcmp(commandLine, "exit") == 0)
+        {
+            break;
+        }
+        else if (strlen(commandLine) == 0)
+        {
+            // empty command, reprompt
+            continue;
+        }
 
         // Parse out command and arguments
         parseCommand(commandLine, &command);
@@ -113,11 +172,22 @@ int main()
         // if command is blank report and continue
         if (command.name == NULL)
         {
-            printf("Command not found\n");
-            continue;
+            printf("\"%s\" command not found\n", command.argv[0]);
+            continue; // reset for next command input
         }
 
-        // TODO: create child process
+        // create child process
+        pid = fork();
+        if (pid == 0)
+        {
+            // Child process
+            execv(command.name, command.argv);
+        }
+        else
+        {
+            // Parent process
+            wait(NULL); // Wait for child to finish executing
+        }
     }
 
     // Success
